b3b2f34993241100120e6fd1d853981b0c118df7
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-09-15 12:34:22 +0300

    make build

---
+++ index.4ea4d17d.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".list-item__link"),t=document.querySelector("#largeImg");e.forEach(function(e){e.addEventListener("click",function(r){r.preventDefault(),t.src=e.href})});
+//# sourceMappingURL=index.4ea4d17d.js.map
